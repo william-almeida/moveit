d1c678a28055dc0aa2af29f27c33d0bd630040a1
william-almeida <william.cntt@gmail.com>
2021-02-23 18:31:36 -0300

    Countdown button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,15 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { Countdown } from "../components/Countdown";
 import styles from '../styles/pages/Home.module.css';
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className={ styles.container }>
+      <Head>
+        <title>Início | Movement</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
