73315959bd550f87e4df5151196c54b2712b3870
william-almeida <william.cntt@gmail.com>
2021-02-25 13:55:56 -0300

    fix problem in experience bar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -6,16 +6,16 @@ export function ExperienceBar() {
   const {currentExperience, experienceToNextLevel} = useContext(ChallengesContext);
 
   const percentToNextLevel = Math.round((currentExperience * 100) / experienceToNextLevel);
-  console.log(percentToNextLevel)
+  console.log('width:', percentToNextLevel)
 
   return(
     <header className={ styles.experienceBar }>
       <span>0 xp</span>
       <div>
-        <div style={{ width: `${percentToNextLevel}`}}></div>
+        <div style={{ width: `${percentToNextLevel}%`}}></div>
         <span
           className={ styles.currentExperience }
-          style={{ left: `${percentToNextLevel}`}}
+          style={{ left: `${percentToNextLevel}%`}}
         > 
           {currentExperience} xp
         </span>
