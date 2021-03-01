c771109bdd12c8b58f655c986101e6e647df4de8
william-almeida <william.cntt@gmail.com>
2021-03-01 13:08:51 -0300

    responsividade

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -22,7 +22,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
 
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(25 * 60);
+  const [time, setTime] = useState(5);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -36,7 +36,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(25 * 60);
+    setTime(5);
     setHasFinished(false);
   }
 
